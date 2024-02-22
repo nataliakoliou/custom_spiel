ddc7333771a5b3304ca876d28a9c992841b312b4
Natalia Koliou <natalykoliou@yahoo.gr>
2024-02-22 18:47:42 +0200

    Fix a couple of issues

--- src/main.py
+++ src/main.py
@@ -18,18 +18,24 @@ for agent in AGENTS:
     size, type, intel, name = agent
     env = Grid(**ENVIRONMENT[size])
     player = Player(**PLAYER(type)[intel])
-    QLearner(**LEARNER(env, player)[name]).run()
+    learner = QLearner(**LEARNER(env, player)[name])
+    learner.run()
 
-"""for pair in PAIRS:
+"""
+for pair in PAIRS:
     size, types, intels, name = pair
     env = Grid(**ENVIRONMENT[size])
-    human = Player(**AGENT(type.h)[intels.h])
-    robot = Player(**AGENT(type.r)[intels.r])
-    Simulator(env, human, robot)[name].run()"""
+    human = Player(**PLAYER(type.h)[intels.h])
+    robot = Player(**PLAYER(type.r)[intels.r])
+    simulator = Simulator(**SIMULATOR(env, human, robot)[name])
+    simulator.run()
+"""
 
-"""metagame = Metagame(
+"""
+metagame = Metagame(
     payoffs = [v.game.payoff for v in versions],
     alpha=1e2,
     profiles=8
 )
-metagame.alpharank()"""
+metagame.alpharank()
+"""
